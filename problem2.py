36b1d0ed97b478fcad1167a2760a4cf5116ac68b
ReticulatedSpline <carpenter.ben@icloud.com>
2019-09-26 16:08:06 -0500

    Added a more nuanced approach to problem 2

--- problem2.py
+++ problem2.py
@@ -1,12 +1,26 @@
-# brute force
-prev = 1
-curr  = 2
-even_sum = 0
+#By considering the terms in the Fibonacci sequence whose values do not exceed four million,
 
-while (curr <= 4000000):
-    if not curr % 2:
-        even_sum += curr
-    print(curr)
-    curr = curr + prev
-    prev = curr - prev
-print("Sum of even fibonacci terms: ", even_sum)
+def naive_approach():
+    prev, curr = 1, 2
+    sum = 0
+
+    while (curr <= 4000000):
+        if not curr % 2:
+            sum += curr
+        curr = curr + prev
+        prev = curr - prev
+    print("Brute force approach: ", sum)
+
+# on the premise that each third number of the fibonacci sequence is even:
+def nuanced_approach():
+    x, y = 1, 1
+    sum = 0
+    while (sum < 4000000):
+        sum += (x + y)
+        new_y = x + 2 * y
+        new_x = 2 * x + 3 * y
+        x, y = new_y, new_x
+    print("Nuanced approach:     ", sum)
+
+naive_approach()
+nuanced_approach()
